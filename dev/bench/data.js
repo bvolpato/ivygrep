40417a9e96dc50001f7810562942073b7b0ded58
github-action-benchmark <github@users.noreply.github.com>
2026-04-05 22:18:28 +0000

    add Rust Benchmark (customSmallerIsBetter) benchmark result for 989f41deb27bb861a8d9af3ca2923beff4eb653f

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1775427508225,
+  "repoUrl": "https://github.com/bvolpato/ivygrep",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "brunocvcunha@gmail.com",
+            "name": "Bruno Volpato",
+            "username": "bvolpato"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "989f41deb27bb861a8d9af3ca2923beff4eb653f",
+          "message": "docs: test benchmark action (#3)\n\n* docs: formatting trigger\n\n* fix: explicitly convert criterion output into custom json benchmark structure\n\n* build: implement robust cargo dependency caching across workflows\n\n* debug benchmark output",
+          "timestamp": "2026-04-05T18:10:50-04:00",
+          "tree_id": "8bc32db12fe32e823c2ef7c9e5148e1f139071f2",
+          "url": "https://github.com/bvolpato/ivygrep/commit/989f41deb27bb861a8d9af3ca2923beff4eb653f"
+        },
+        "date": 1775427507802,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "indexer/index_small_workspace",
+            "value": 691021171.6,
+            "unit": "ns"
+          }
+        ]
+      }
+    ]
+  }
+}
