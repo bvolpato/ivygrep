2648fb91970c22b18025f9ddcb6c0e5b5edaf625
github-action-benchmark <github@users.noreply.github.com>
2026-04-05 22:22:38 +0000

    add Rust Benchmark (customSmallerIsBetter) benchmark result for 8104515f79cc769ca6c00408894b12e4a77b02a2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775427508225,
+  "lastUpdate": 1775427758653,
   "repoUrl": "https://github.com/bvolpato/ivygrep",
   "entries": {
     "Rust Benchmark": [
@@ -28,6 +28,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "indexer/index_small_workspace",
             "value": 691021171.6,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "brunocvcunha@gmail.com",
+            "name": "Bruno Volpato",
+            "username": "bvolpato"
+          },
+          "committer": {
+            "email": "brunocvcunha@gmail.com",
+            "name": "Bruno Volpato",
+            "username": "bvolpato"
+          },
+          "distinct": true,
+          "id": "8104515f79cc769ca6c00408894b12e4a77b02a2",
+          "message": "chore: remove debug output from benchmarks",
+          "timestamp": "2026-04-05T18:21:08-04:00",
+          "tree_id": "789e37bed2a0dc975186c27ee27481894d7a8b06",
+          "url": "https://github.com/bvolpato/ivygrep/commit/8104515f79cc769ca6c00408894b12e4a77b02a2"
+        },
+        "date": 1775427757512,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "indexer/index_small_workspace",
+            "value": 710680738.8,
             "unit": "ns"
           }
         ]
